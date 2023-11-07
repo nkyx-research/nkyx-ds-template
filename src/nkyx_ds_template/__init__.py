585856e6066e93cd61407d463211900f05cca132
tangqu <tangqu@zepp.com>
2023-11-07 14:58:48 +0800

    Clean up to simplify the template structure

---
+++ src/nkyx_ds_template/__init__.py
@@ -0,0 +1,3 @@
+"""NKYX data science template"""
+
+__version__ = "0.0.1"
---
+++ src/nkyx_ds_template/hello_world.py
@@ -0,0 +1,7 @@
+"""This is a Sample Python file."""
+
+
+def hello_world(i: int = 0) -> str:
+    """Doc String."""
+    print("hello world")
+    return f"string-{i}"
